b1fcccee85f737f084dded63741f9224472d3051
Alex Lemons <alexlemons1@gmail.com>
2015-11-04 15:39:06 +0000

    consultation routes add and tested with postman

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ router.route('/users/:user_id')
         res.send(err);
       user.name = req.body.name;  // update the users info
 
-    // save the bear
+    // save the user
       user.save(function(err) {
       if (err)
          res.send(err);
@@ -138,6 +138,61 @@ router.route('/consultations')
           res.json({ message: 'Consultation created!' });
       });
   })
+
+  // get all the consultations (accessed at GET http://localhost:8080/api/consultations)
+  .get(function(req, res) {
+    Consultation.find(function(err, consultations) {
+        if (err)
+          res.send(err);
+
+        res.json(consultations);
+    });
+  });
+
+router.route('/consultations/:consultation_id')
+
+  // get the consultation with that id (accessed at GET http://localhost:8080/api/consultations/:consultation_id)
+  .get(function(req, res) {
+    Consultation.findById(req.params.consultation_id, function(err, consultation) {
+      if (err)
+        res.send(err);
+      res.json(consultation);
+    });
+  })
+
+  // update the consultation with this id (accessed at PUT http://localhost:8080/api/consultations/:consultation_id)
+  .put(function(req, res) {
+
+    // use our consultation model to find the consultation we want
+    Consultation.findById(req.params.consultation_id, function(err, consultation) {
+      if (err)
+        res.send(err);
+      consultation.description = req.body.description; //update consultation data
+      consultation.patientID = req.body.patientID;
+      consultation.drID = req.body.drID;
+
+    // save the consultation
+      consultation.save(function(err) {
+      if (err)
+         res.send(err);
+
+      res.json({ message: 'Consultation updated!' });
+      });
+    });
+  })
+
+  // delete the consultation with this id (accessed at DELETE http://localhost:8080/api/consultations/:consultation_id)
+  .delete(function(req, res) {
+
+    Consultation.remove({
+      _id: req.params.consultation_id
+    }, function(err, consultation) {
+      if (err)
+        res.send(err);
+
+      res.json({ message: 'Successfully deleted' });
+    });
+  });
 
 
 // REGISTER OUR ROUTES -------------------------------
